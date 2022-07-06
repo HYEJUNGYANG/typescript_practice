d058a1998a2445a43e876f340ddb9c7976ea9a56
양혜정 <gpwjd248@gmail.com>
2022-07-07 04:58:00 +0900

    typescript느낌 파악

---
+++ typescript/main.js
@@ -0,0 +1,9 @@
+// console.log()는 js에도 존재하기 때문에 node로 정상 출력이 가능함
+console.log('Hello World~!');
+// node에서 typescript 코드를 이해할 수 없음
+var Car = /** @class */ (function () {
+  function Car() {
+    this.engine = 1;
+  }
+  return Car;
+})();
