61f41bad1fbe99a58aa17f94944e3b6a24efaaee
양혜정 <102224554+HYEJUNGYANG@users.noreply.github.com>
2022-07-07 05:37:15 +0900

    Delete main.ts

--- typescript/main.ts
+++
@@ -1,9 +0,0 @@
-// console.log()는 js에도 존재하기 때문에 node로 정상 출력이 가능함
-console.log('Hello World~!');
-// node에서 typescript 코드를 이해할 수 없음
-class Car {
-  engine: number;
-  constructor() {
-    this.engine = 1;
-  }
-}
