3590f55cd4a868a934bdf5da2e2b060eaeb2ba5c
양혜정 <gpwjd248@gmail.com>
2024-05-15 21:24:15 +0900

    다시 공부하면서 1-3-array.ts 부분 추가

--- typescript/1-types/1-3-array.ts
+++ typescript/1-types/1-3-array.ts
@@ -3,10 +3,11 @@
   // 타입 정의 방법 2개
   const fruits: string[] = ['🍅', '🍌'];
   const scores: Array<number> = [1, 2, 3];
-  // 주어진 데이터를 변경 및 업데이트 불가능 (readonly 뒤에 Array<string>은 x)
+  // 주어진 데이터를 변경 및 업데이트 불가능 (readonly 뒤에 Array<string> 방식은 사용 불가능)
   function printArray(fruits: readonly string[]) {}
 
-  // Tuple -> interface, type alias, class 등으로 대체가능
+  // Tuple -> 서로 다른 타입의 데이터를 담을 수 있음 / interface, type alias, class 등으로 대체가능
+  // 사용 권장은 X
   // 동적으로 return하는데 클래스, 인터페이스 등으로 묶기 애매하고 동적으로 관련있는 다른 타입의 데이터를 묶어서 이름을 정의해서 쓸 경우가 아니면 대체 가능여부 생각해보는 것이 좋음
   let student: [string, number];
   student = ['name', 123];
