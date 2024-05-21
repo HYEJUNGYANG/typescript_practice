d50f533a8beb43bf1fda80e579b3abd360f144c4
양혜정 <gpwjd248@gmail.com>
2024-05-22 03:07:13 +0900

    interface

--- typescript/3-oop/3-4-abstraction.ts
+++ typescript/3-oop/3-4-abstraction.ts
@@ -8,10 +8,16 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
   // CoffeeMachine이라는 클래스는 CoffeeMaker(인터페이스)의 규격을 따라가고 이것을 구현하는 클래스
   // 인터페이스를 구현(implements)하는 클래스에서는 interface에 적혀있는 모든 함수를 구현해야함
   // 인터페이스를 이용하면 추상화를 좀 더 극대화해서 사용가능
-  class CoffeeMachine implements CoffeeMaker {
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     private static BEANS_CRAM_PER_SHOT: number = 7;
     private coffeeBeans: number = 0;
 
@@ -28,6 +34,10 @@
         throw new Error('커피콩의 크기는 0보다 커야해!');
       }
       this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log('cleaning the machine...🧼');
     }
 
     private grindBeans(shots: number) {
@@ -57,10 +67,28 @@
     }
   }
 
-  const maker1: CoffeeMachine = CoffeeMachine.makeMachine(12);
-  maker1.fillCoffeeBeans(32);
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
 
-  // CoffeeMaker에는 fillCoffeeBeans라는 함수가 없기 때문에 사용 불가능
-  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(12);
-  //   maker2.fillCoffeeBeans(32);
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
+  // 동일한 오브젝트의 인스턴스일지라도
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  // 각각 다른 인터페이스를 구현하기 때문에 접근 가능한 함수가 달라짐
+  const amateur = new AmateurUser(maker);
+  const pro = new ProBarista(maker);
+  pro.makeCoffee();
 }
